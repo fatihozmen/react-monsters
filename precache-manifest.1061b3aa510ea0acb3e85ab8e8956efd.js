5b3dfb5d9333491cd6c0224ba9e93f92e83a9bd1
fatihozmen <fatih.ozmen@hotmail.com.tr>
2020-04-10 15:05:38 +0100

    Updates

---
+++ precache-manifest.1061b3aa510ea0acb3e85ab8e8956efd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18104c4016ffce4593ed44e38207fcd",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "d74c772304b55351fef3",
+    "url": "/react-monsters/static/css/main.3c8c8c4d.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/react-monsters/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d74c772304b55351fef3",
+    "url": "/react-monsters/static/js/main.b3f4f8d9.chunk.js"
+  },
+  {
+    "revision": "e266b3d6e293286279ed",
+    "url": "/react-monsters/static/js/runtime-main.5c609429.js"
+  }
+]);
